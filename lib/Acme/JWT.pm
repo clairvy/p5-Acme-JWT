469ea805a56f48888dcf35edc62b0babe5d57ea8
clairvy <clairvy@gmail.com>
2011-11-24 00:35:38 +0900

    initial commit

---
+++ lib/Acme/JWT.pm
@@ -0,0 +1,32 @@
+package Acme::JWT;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::JWT -
+
+=head1 SYNOPSIS
+
+  use Acme::JWT;
+
+=head1 DESCRIPTION
+
+Acme::JWT is
+
+=head1 AUTHOR
+
+NAGAYA Shinichiro E<lt>clairvy@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
